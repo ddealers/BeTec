640f73c431e2089b831636153bf034bb66c533e9
vampaynani <vampay@digitaldealers.mx>
2014-10-13 05:06:14 -0500

    beta testing

--- js/main.js
+++ js/main.js
@@ -16,6 +16,57 @@ $(document).ready(function(){
 	$(window).on('resize', updateContainer);
 	updateContainer();
 
+	//===== Navigation =====
+	var sections = ['be_tec','actividades','agenda','sede','registrate','faq'];
+	var sindex = 0;
+	$('.arrows .next').on('click', function(e){
+		e.preventDefault();
+		sindex++;
+		if(sindex >= sections.length){
+			sindex = 0;
+		}
+		var sel = $('header nav ul li').eq(sindex).children('a');
+		skrollr.menu.click(sel[0]);
+	});
+	$('.arrows .prev').on('click', function(e){
+		e.preventDefault();
+		sindex--;
+		if(sindex < 0){
+			sindex = sections.length - 1;
+		}
+		var sel = $('header nav ul li').eq(sindex).children('a');
+		skrollr.menu.click(sel[0]);
+	});
+	$('header nav ul li a').on('click', function(e){
+		$('#end').hide();
+	});
+	//===== Form =====
+	var index_form = 0;
+	setFormStep(0);
+
+	$("#registro .next").on("click", function(e){
+		e.preventDefault();
+		prev = index;
+		index++;
+		if(index > 3){
+			$('#end').show();
+			//alert("registro completo");
+			TweenMax.from('#end', 1, {opacity: 0, scale: 0.5, ease: Back.easeOut});
+			return;
+		}
+		setFormStep(index);
+	});
+
+	function setFormStep(next){
+		$(".step").hide();
+
+		var ns = $(".step").eq(next);
+		
+		ns.show();
+		
+		TweenMax.from(ns, 0.3, {opacity: 0, y: 100});
+	}
+
 	//===== Calendar =====
 	var index = 0;
 	setCalendarDay(0);
@@ -56,6 +107,6 @@ $(document).ready(function(){
 	//===== Tables =====
 	var sx = 1366 / $(window).width();
 	var sy = 864 / $(window).height();
-	$("table.jueves").css({width: 850 * sx, height: 130 * sy});
-	$("table.viernes").css({width: 1020 * sx, height: 440 * sy});
+	//$("table.jueves").css({width: 850 * sx, height: 130 * sy});
+	//$("table.viernes").css({width: 1020 * sx, height: 440 * sy});
 });
