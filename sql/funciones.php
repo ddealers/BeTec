979b506257cd37b1ef81811cce7687f43db29317
vampaynani <vampay@digitaldealers.mx>
2014-10-20 13:57:40 -0500

    cambios correo Germán

--- sql/funciones.php
+++ sql/funciones.php
@@ -22,7 +22,7 @@ if(isset($_GET['opc']) && $_GET['opc'] == 'estadoCity'){
 	
 	$idc = $_GET['idc'];
 	$mysqli = new mysqli(HOST,USR,PWD,DB);
-	$prim = "<option value=''>Prepa donde estudias**</option>";
+	$prim = "<option value=''>Prepa donde estudias*</option>";
 
 	if(isset($idc) && $idc != NULL){
 		$q = "SELECT id, nombre FROM preparatorias WHERE id_ciudad = $idc";
@@ -30,7 +30,7 @@ if(isset($_GET['opc']) && $_GET['opc'] == 'estadoCity'){
 
 		if($v){
 			while ($row = $v->fetch_assoc()) {
-				$listado .= ucwords("<option value='".$row['id']."'>".$row['nombre']."</option>");
+				$listado .= utf8_encode(ucwords("<option value='".$row['id']."'>".$row['nombre']."</option>"));
 			}
 		}
 	}
