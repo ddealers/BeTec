d6f1b3ae8ea7f14b17e46e3dd880b31314d66a75
Juan Francisco <fherrera@softin.mx>
2014-11-03 18:32:37 -0600

    new header

--- admin/header.php
+++ admin/header.php
@@ -1,4 +1,7 @@
-<?php session_start(); ?>
+<?php 
+session_start(); 
+
+?>
 
 <!DOCTYPE html>
 <html lang="es">
@@ -7,10 +10,19 @@
 	<title>BTEC</title>
 	<link rel="stylesheet" type="text/css" href="./bootstrap/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="./bootstrap/css/bootstrap-theme.min.css">
+	<script type="text/javascript" src="../js/vendor/jquery.min.js"></script>
 	<script type="text/javascript" src="./bootstrap/js/bootstrap.min.js"></script>
+	<script type="text/javascript">
+	$(document).ready(function(){
+		$('#myTab a').click(function (e) {
+			e.preventDefault()
+			$(this).tab('show')
+		});
+	});
+	</script>
 </head>
 <body>
-<div class="container">
-	<nav class="navbar navbar-default">
-		<img src="../img/logo.png">
-	</nav>
+	<div class="container">
+		<nav class="navbar navbar-default">
+			<img src="../img/logo.png">
+		</nav>
--- admin/index.php
+++ admin/index.php
@@ -1,31 +1,9 @@
 <?php 
+
 require_once('header.php');
 if(isset($_SESSION['user']) && $_SESSION['user'] != NULL){
 //echo $_SESSION['user']->s_login_user;
 ?>
-<!DOCTYPE html>
-<html lang="es">
-<head>
-	<meta charset="UTF-8">
-	<title>BTEC</title>
-	<link rel="stylesheet" type="text/css" href="./bootstrap/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="./bootstrap/css/bootstrap-theme.min.css">
-	<script type="text/javascript" src="../js/vendor/jquery.min.js"></script>
-	<script type="text/javascript" src="./bootstrap/js/bootstrap.min.js"></script>
-	<script type="text/javascript">
-	$(document).ready(function(){
-		$('#myTab a').click(function (e) {
-			e.preventDefault()
-			$(this).tab('show')
-		});
-	});
-	</script>
-</head>
-<body>
-	<div class="container">
-		<nav class="navbar navbar-default">
-			<img src="../img/logo.png">
-		</nav>
 		<!--form role="form">
   			<div class="form-group">
     			<label for="exampleInputEmail1">Email address</label>
