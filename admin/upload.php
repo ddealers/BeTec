4a1b05592e67c7dc238d79fd0758816a3f4a6e39
Juan Francisco <fherrera@softin.mx>
2014-11-06 01:38:15 -0600

    upload full

--- admin/upload.php
+++ admin/upload.php
@@ -1,15 +1,46 @@
 <?php
+date_default_timezone_set('America/Mexico_City');
+require_once("config.php");
 
 $action = $_POST['action'];
 $uid 	= $_POST['id'];
 
+$hora = date('YmdHms');
+$ruta = '../download/';
+
+$mysqli = new mysqli(HOST,USR,PWD,DB);
+
 if(isset($action) AND $action == 'carta'){
-	$carta 	= $_FILES['cartacomp'];
-	//pedo pa' subir y guardar
+	$e = 69;
+	if(isset($_FILES['cartacomp']) && $_FILES['cartacomp'] != NULL){
+		$uname = str_replace(' ', '', $_FILES['cartacomp']['name']);
+		$name0 = $hora . $uname;
+		$mv0 = move_uploaded_file($_FILES['cartacomp']['tmp_name'], $ruta.$name0);
+		
+		$q = "UPDATE usuarios_documentos SET url_permiso = '$name0' WHERE id_usuario = '$uid' ";
+		$v = $mysqli->query($q);
+		if($v){
+			$e = 100; 
+		}
+	}
+
 }elseif (isset($action) AND $action == 'pago') {
-	$pago 	= $_FILES['compago'];
-	//
+	$e = 69;
+	if(isset($_FILES['compago']) && $_FILES['compago'] != NULL){
+		$uname1 = str_replace(' ', '', $_FILES['compago']['name']);
+		$name1 = $hora . $uname1;
+		$mv1 = move_uploaded_file($_FILES['compago']['tmp_name'], $ruta.$name1);
+		
+		$q = "UPDATE usuarios_documentos SET url_pago = '$name1' WHERE id_usuario = '$uid' ";
+		$v = $mysqli->query($q);
+		if($v){
+			$e = 101;
+		}
+	}
 }
+
+header("Location: index.php?u=".$uid."&e=".$e);
+
 
 
 ?>
