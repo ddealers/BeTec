38a1f6ab04048d65f0386b9e517ba2db2f68ee98
vampaynani <vampay@digitaldealers.mx>
2014-11-14 16:02:48 -0600

    excel & docs

--- admin/config.php
+++ admin/config.php
@@ -1,8 +1,8 @@
 <?php
 define('HOST', 'localhost');
-define('USR', 'root');
-define('PWD', '');
-define('DB', 'betec_2014');
+define('USR', 'tec_server');
+define('PWD', '*BtBTec1411?');
+define('DB', 'betec');
 /*
 define('HOST', 'localhost');
 define('USR', 'root');
