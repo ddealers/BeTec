3860cf8b16a6bd699bdab2c60839ef114e4c31e3
vampaynani <vampay@digitaldealers.mx>
2014-11-14 00:37:52 -0600

    config.php

---
+++ admin/config.php
@@ -0,0 +1,12 @@
+<?php
+define('HOST', 'localhost');
+define('USR', 'tec');
+define('PWD', 'tec12345');
+define('DB', 'betec');
+/*
+define('HOST', 'localhost');
+define('USR', 'root');
+define('PWD', 'olamund0');
+define('DB', 'test');
+*/
+?>
