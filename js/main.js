0ebcbcff2261081938d75d1c54d058599098824a
vampaynani <vampay@digitaldealers.mx>
2014-11-19 00:27:05 -0600

    vampay changes

--- js/main.js
+++ js/main.js
@@ -103,6 +103,8 @@ $(document).ready(function () {
 	//===== Registro =====
 	function initRegistro(e){
 		e.preventDefault();
+		alert("Gracias por tu interés, el registro se ha terminado. Te esperamos en BTEC 2015.");
+		return;
 		carrerasList = {carrera1:null, carrera2: null, carrera3: null};
 		viernesList = {vopt1:null, vopt2: null, vopt3: null};
 		sabadoList = {sopt1:null, sopt2: null};
